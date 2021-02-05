043e5efaa76aca4d8e0e2b78d12d1d70528e292e
kenerase <70031940+kenerase@users.noreply.github.com>
2021-02-05 21:19:50 +0300

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,5 +1,4 @@
 from cryptography.fernet import Fernet 
-import os 
 
 # def generate_key():
 # 	key = Fernet.generate_key()
@@ -7,12 +6,14 @@ import os
 # 	file.write(key)
 # 	file.close()
 
+
 file = open('key.key', 'rb')
 key = file.read()
 file.close()
 
 
-path = input('Enter Name Folder: ')
+
+filename = input('Enter FileName: ')
 def asking():
 	enc_dec = input('What You Want To Do?: [1] - encrypt, [2] - decrypt: ')
 	if enc_dec == '1':
@@ -23,27 +24,20 @@ def asking():
 
 def encrypt():
 	f = Fernet(key)
-	names = os.listdir(path)
-	for name in names:
-		fullname = os.path.join(path, name)
-		if os.path.isfile(fullname):
-			with open(fullname, 'rb') as file:
-				file_data = file.read()
-			encrypted_data = f.encrypt(file_data)	
-			with open(fullname, 'wb') as file:
-				file.write(encrypted_data)
+	with open(filename, 'rb') as file:
+		file_data = file.read()
+	encrypted_data = f.encrypt(file_data)
+	with open(filename, 'wb') as file:
+		file.write(encrypted_data)
 
+	
 def decrypt():
 	f = Fernet(key)
-	names = os.listdir(path)
-	for name in names:
-		fullname = os.path.join(path, name)
-		if os.path.isfile(fullname):
-			with open(fullname, 'rb') as file:
-				data = file.read()
-			decrypted_data = f.decrypt(data)
-			with open(fullname, 'wb') as file:
-				file.write(decrypted_data)
-
+	with open(filename, 'rb') as file:
+		file_data = file.read()
+	decrypted_data = f.decrypt(file_data)
+	with open(filename, 'wb') as file:
+		file.write(decrypted_data)
 
 asking()
+
